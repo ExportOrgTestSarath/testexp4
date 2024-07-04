26ad544a8cc9652aab0d09850e7d6e77d6920ac5
rsarathkish <131669079+rsarathkish@users.noreply.github.com>
2024-07-05 02:42:37 +0530

    Commit for the generated/updated files via system

---
+++ Frontend/base/app/auth/prototype.variables.ts
@@ -0,0 +1,5 @@
+export class PrototypeVariables {
+	public static APP_ID = "fc073ac4-2139-4648-ad98-19c96f598a5d";
+	public static TENANT_ID = "2cb92d98-aa02-4a6b-a0af-f4ebfbded16f";
+	public static DESIGN_STUDIO_URL = "https://eva-design-studio-rsarath-export-testing-designst-vkmhkpuxwq-ez.a.run.app/";
+}
